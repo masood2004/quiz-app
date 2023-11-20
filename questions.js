1389647a987003023d717f2c70a0bd82c7f69c97
Syed Masood Hussain <hmasood3288@gmail.com>
2023-11-21 00:14:39 +0500

    Initial commit and complete project

---
+++ questions.js
@@ -0,0 +1,38 @@
+export const questions = [
+    {
+        question: "Which is the largest animal in the world?",
+        answer: [
+            { text: "Shark", correct: false},
+            { text: "Blue Whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answer: [
+            { text: "Vatican city", correct: true},
+            { text: "Bhutan", correct: false},
+            { text: "Nepal", correct: false},
+            { text: "Sri Lanka", correct: false},
+        ]
+    },
+    {
+        question: "Which is the largest dessert in the world?",
+        answer: [
+            { text: "Kalahari", correct: false},
+            { text: "Gobi", correct: false},
+            { text: "Sahara", correct: false},
+            { text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answer: [
+            { text: "Asia", correct: false},
+            { text: "Australia", correct: true},
+            { text: "Arctic", correct: false},
+            { text: "Africa", correct: false},
+        ]
+    },
+];
